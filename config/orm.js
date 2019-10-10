5adb4ee83c0b9010cc04a463fb3982352029117c
Ryan Goderis <rgoderis789@gmail.com>
2019-10-10 18:25:01 -0400

    set function to object key value pairs to syntax, set function updateOne to update burgers table

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,28 @@
 var con = require("../config/connection.js");
 
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+    var arr = [];
+  
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+      var value = ob[key];
+      // check to skip hidden properties
+      if (Object.hasOwnProperty.call(ob, key)) {
+        // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        }
+        // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+        // e.g. {sleepy: true} => ["sleepy=true"]
+        arr.push(key + "=" + value);
+      }
+    }
+  
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+  }
+
 var orm = {
     // orm function to select all from burgers
     selectAll: function(table, cb){
@@ -20,9 +43,15 @@ var orm = {
         });
     },
     // orm function to update val in burgers
-    updateOne: function(){
-
+    updateOne: function(table, objColVals, condition, cb){
+        var queryString = "UPDATE " + table;
+        queryString += " SET " + objToSql(objColVals);
+        queryString += " WHERE " + condition;
+        con.query(queryString, function(err, result){
+            if(err) throw err;
+            cb(result);
+        });
     }
 }
 
-module.exports = orm
+module.exports = orm;
