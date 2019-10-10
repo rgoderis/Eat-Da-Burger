77daa796db9aeb0d723079156da279dd64e8cf10
Ryan Goderis <rgoderis789@gmail.com>
2019-10-10 16:11:56 -0400

    required npms, set middleware for express and handlebars, set routes and server to listen on process.env or port 3000

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require("express");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+// serve static content for the app from the public directory
+app.use(express.static("public"));
+
+// parse body requests as JSON
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// set handlebars
+var exhbs = require("express-handlebars");
+
+app.engige("handlebars", exhbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+// import routes and give server acces
+var routes = require("./controllers/burgerController");
+
+app.use(routes);
+
+app.listen(PORT, function(){
+    console.log("Listening on PORT: " + PORT)
+})
