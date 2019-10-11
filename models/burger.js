ab3165b6b61af8ed53a76e64450d36d71236f8fd
Ryan Goderis <rgoderis789@gmail.com>
2019-10-11 17:02:11 -0400

    set orms in burger.js to select, insert, and update with the burgers table

--- models/burger.js
+++ models/burger.js
@@ -1,11 +1,26 @@
+// allow access to orms created in orm.js
 var orm = require("../config/orm.js");
 
+// set orms in burger
 var burger = {
-    selectAll: fucntion(cb){
-        orm.selectAll("burgers", function(res){
-            cb(res);
+    // orm to select all from burgers table
+    selectAll: function(cb){
+        orm.insertOne("burgers", function(res){
+            cb(res)
+        });
+    }, 
+    // orm to insert new row to burgers table
+    insertOne: function(col, val, cb){
+        orm.insertOne("burgers", col, val, function(res){
+            cb(res)
         });
     },
+    // orm to update val in burgers table
+    updateOne: function(objColVals, condition, cb){
+        orm.updateOne("burgers", objColVals, condition, function(res){
+            cb(res)
+        });
+    }
 }
 
 module.exports = burger;
